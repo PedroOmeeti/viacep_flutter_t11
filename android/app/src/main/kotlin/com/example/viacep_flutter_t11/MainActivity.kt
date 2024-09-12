863c81985bf64a3237713576ead8f672a4929e5a
Pedro Henrique <124195001+PedroOmeeti@users.noreply.github.com>
2024-09-12 17:09:52 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/viacep_flutter_t11/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.viacep_flutter_t11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:viacep_flutter_t11/view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeView(),
+    );
+  }
+}
+
